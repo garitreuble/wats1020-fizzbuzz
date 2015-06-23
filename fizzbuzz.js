8de066c86e5bea531d56425f74ac6049f74033e2
garitreuble <garitreuble@me.com>
2015-06-23 12:46:49 -0400

    notes added

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,12 +6,16 @@ var responseFor5 = 'Pancakes';
 for (i=1; i<=rangeLimit; i++) {
 		if (i % 3 === 0) {
 			if (i % 5 === 0) {
+				//If the number is divisible by **BOTH** 3 and 5, print "FizzBuzz" to the console.
 			document.write(responseFor3+responseFor5+"<BR>");
 		} else {
 			document.write(responseFor3+"<BR>");
+			//If the number is divisible by 3, print "Fizz" to the console.
 		}
 	} else if (i % 5 === 0) {
+					//If the number is divisible by 5, print "Buzz" to the console.
 		document.write(responseFor5+"<BR>");
+		//If none of these conditions are met, print the number to the console.
 	} else {
 		document.write(i+"<BR>");
 	}
