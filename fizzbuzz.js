215492ac670a7ea3982c336e166911230e5d4373
garitreuble <garitreuble@me.com>
2015-06-23 12:36:44 -0400

    fizzbuzz completion

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,18 @@
 // Place your FizzBuzz code here. 
+var rangeLimit = 1000;
+var responseFor3 = 'Bacon';
+var responseFor5 = 'Pancakes';
+
+for (i=1; i<=rangeLimit; i++) {
+		if (i % 3 === 0) {
+			if (i % 5 === 0) {
+			document.write(responseFor3+responseFor5+"<BR>");
+		} else {
+			document.write(responseFor3+"<BR>");
+		}
+	} else if (i % 5 === 0) {
+		document.write(responseFor5+"<BR>");
+	} else {
+		document.write(i+"<BR>");
+	}
+}
